b6ca847b5350d671344f3fc32b3411cfada86ae3
Aleksey <aleksey91scorp@bk.ru>
2020-07-17 14:33:45 +0300

    mongoose and mongodb

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const morgan = require("morgan");
-
-// Route files
-const bootcamps = require("./routes/bootcamps");
+const connectDB = require("./config/db");
 
 //Load env vars
 dotenv.config({ path: "./config/config.env" });
+
+// Connect to database
+connectDB();
+
+// Route files
+const bootcamps = require("./routes/bootcamps");
 
 const app = express();
 
@@ -20,7 +24,15 @@ app.use("/api/v1/bootcamps", bootcamps);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(
+const server = app.listen(
   PORT,
   console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
 );
+
+// Handle unhandled promise rejections
+process.on("unhandledRejection", (err, promise) => {
+    console.error(`Error: ${err.message}`);
+
+    //Close server & expit process
+    server.close(() => process.exit(1));
+})

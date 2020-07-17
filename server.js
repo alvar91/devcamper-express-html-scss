1e1217310ba34463d57abd125acb30e0a0775814
Aleksey <aleksey91scorp@bk.ru>
2020-07-17 13:23:20 +0300

    morgan logger

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const dotenv = require("dotenv");
-const logger = require("./middlewares/logger");
+const morgan = require("morgan");
 
 // Route files
 const bootcamps = require("./routes/bootcamps");
@@ -10,9 +10,10 @@ dotenv.config({ path: "./config/config.env" });
 
 const app = express();
 
-
-
-app.use(logger);
+// Dev logging middlewares
+if(process.env.NODE_ENV === "development") {
+    app.use(morgan("dev"));
+}
 
 // Mount routers
 app.use("/api/v1/bootcamps", bootcamps);

8bc893b2b60a72638b063e9ae71548c97fd10def
Aleksey <aleksey91scorp@bk.ru>
2020-07-17 12:58:58 +0300

    main routes

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 const express = require("express");
 const dotenv = require("dotenv");
 
+// Route files
+const bootcamps = require("./routes/bootcamps");
+
 //Load env vars
 dotenv.config({ path: "./config/config.env" });
 
 const app = express();
+
+// Mount routers
+app.use("/api/v1/bootcamps", bootcamps);
 
 const PORT = process.env.PORT || 5000;
 
